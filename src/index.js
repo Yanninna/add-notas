60609e7d2714a0ade265b4dfd498b6c6408674a8
yanninna <53129565+yanninna@users.noreply.github.com>
2020-09-19 19:54:45 -0300

    version estable de la viste , lo cargado en la base

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
  const express = require('express');
  const path = require('path');
  const exphbs = require('express-handlebars');
+ const Handlebars = require('handlebars')
  const methodOverride = require('method-override');
  const session = require('express-session');
-
+ const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
 
  //initiliazations 
  const app = express();
@@ -17,6 +18,7 @@
  app.engine('.hbs', exphbs({
      //se crea una especie de plantilla para el marco 
      defaultLayout:"main",
+     handlebars: allowInsecurePrototypeAccess(Handlebars),
      layoutsDir: path.join(app.get('views'), 'layouts'),
      partialsDir: path.join(app.get('views'), 'partials'),
      extname: '.hbs'
