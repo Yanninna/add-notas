8a4fa96b62b893acb405410f69945da0cf9ed4ee
yanninna <53129565+yanninna@users.noreply.github.com>
2020-09-24 17:47:33 -0300

    version estable del a app

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/users');
  
 router.get('/users/signin', (req, res) => {
     res.render('users/signin');
@@ -7,6 +8,32 @@ router.get('/users/signin', (req, res) => {
  
 router.get('/users/signup', (req, res) => {
     res.render('users/signup');
+});
+router.post('/users/signup', async (req, res) => {
+    const { name, email, password, Confirme_password } = req.body;
+    const errors = [];
+   
+    if (name.length <= 0 ) {
+    errors.push({text: 'Ingrese un nombre'});
+   }
+
+    if (password != Confirme_password) {
+   errors.push({text: 'su contraseña no coinside'});
+    }
+    if (password.length < 4 ) {
+        errors.push({text: 'su contraseña debe ser mayor a 4 digitos'});
+    }
+    if (errors.length > 0 ) {
+        res.render('users/signup', {errors, name, email, password, Confirme_password });
+    } else {
+        
+     const newUser =  new User({name, email, password});
+     
+     await newUser.save();
+     req.flash('success_msg', 'Te Registraste Con Exito');
+    
+    }
+   
 });
 
 
